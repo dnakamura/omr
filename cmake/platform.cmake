1fc6149c6ad362ed398afe6f56a073ff3879b54d
Devin Nakamura <devinn@ca.ibm.com>
2017-03-10 18:02:37 -0500

    Initial z/OS CMake support

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -32,6 +32,11 @@ elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm")
 	set(OMR_ARCH_ARM ON)
 	set(OMR_ENV_DATA32 ON)
 	set(OMR_TARGET_DATASIZE 32)
+elseif(CMAKE_SYSTEM_NAME MATCHES "OS390")
+	set(OMR_ARCH_S390 ON)
+	set(OMR_ENV_DATA32 ON)
+	set(OMR_TARGET_DATASIZE 32)
+	set(OMR_HOST_OS "zos")
 else()
 	message(FATAL_ERROR "Unknown processor: ${CMAKE_PROCESSOR}")
 endif()
@@ -43,6 +48,8 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	set(OMR_HOST_OS "osx")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 	set(OMR_HOST_OS "win")
+elseif(OMR_HOST_OS STREQUAL "zos")
+	# we already set up OS stuff above
 else()
 	message(FATAL_ERROR "Unsupported OS: ${CMAKE_SYSTEM_NAME}")
 endif()
@@ -83,6 +90,9 @@ if(OMR_HOST_OS STREQUAL "osx")
 	)
 endif()
 
+# interface library for exporting symbols from dynamic library
+# currently does nothing except on zos
+add_library(omr_shared INTERFACE)
 
 if(OMR_HOST_OS STREQUAL "win")
 	#TODO: should probably check for MSVC
@@ -150,6 +160,96 @@ if(OMR_HOST_OS STREQUAL "win")
 	#Hack up output dir to fix dll dependency issues on windows
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
 endif()
+
+if(OMR_HOST_OS STREQUAL "zos")
+
+	enable_language(ASM-ZOS)
+
+	include_directories(util/a2e/headers)
+
+	add_definitions(
+		-DJ9ZOS390
+		-DLONGLONG
+		-DJ9VM_TIERED_CODE_CACHE
+		-D_ALL_SOURCE
+		-D_XOPEN_SOURCE_EXTENDED
+		-DIBM_ATOE
+		-D_POSIX_SOURCE
+	)
+
+	#NOTE: character escaping on compiler args seems broken
+
+	# Global Flags
+	# xplink   Link with the xplink calling convention
+	# convlit  Convert all string literals to a codepage
+	# rostring Place string literals in read only storage
+	# FLOAT    Use IEEE (instead of IBM Hex Format) style floats
+	# enum     Specifies how many bytes of storage enums occupy
+	# a,goff   Assemble into GOFF object files
+	# NOANSIALIAS Do not generate ALIAS binder control statements
+	# TARGET   Generate code for the target operating system
+	# list     Generate assembly listing
+	# offset   In assembly listing, show addresses as offsets of function entry points
+	#set(OMR_GLOBAL_FLAGS "-Wc,xplink,convlit\\\(ISO8859-1\\\),rostring,FLOAT\\\(IEEE,FOLD,AFP\\\),enum\\\(4\\\) -Wa,goff -Wc,NOANSIALIAS -Wc,TARGET\\\(zOSV1R13\\\) -W \"c,list,offset\"")
+	set(OMR_GLOBAL_FLAGS "-Wc,xplink,convlit\\\(ISO8859-1\\\),rostring,FLOAT\\\(IEEE,FOLD,AFP\\\),enum\\\(4\\\) -Wa,goff -Wc,NOANSIALIAS -Wc,TARGET\\\(zOSV1R13\\\)")
+
+	if(OMR_ENV_DATA64)
+		add_definitions(-DJ9ZOS39064)
+		set(OMR_GLOBAL_FLAGS "${OMR_GLOBAL_FLAGS} -Wc,lp64 -Wa,SYSPARM\\\(BIT64\\\)")
+	else()
+		add_definitions(-D_LARGE_FILES)
+	endif()
+
+
+	set(CMAKE_ASM-ZOS_FLAGS "${OMR_GLOBAL_FLAGS}")
+	set(OMR_GLOBAL_FLAGS "${OMR_GLOBAL_FLAGS} -qnosearch -I${CMAKE_SOURCE_DIR}/util/a2e/headers -I/usr/include")
+	# Specify the minimum arch for 64-bit programs
+	#TODO this is a gross hack
+
+	set(CMAKE_C_FLAGS "-Wc,ARCH\\\(7\\\) -Wc,\"langlvl(extc99)\" ${OMR_GLOBAL_FLAGS}")
+	set(CMAKE_CXX_FLAGS "-Wc,ARCH\\\(7\\\) -Wc,\"langlvl(extended)\" ${OMR_GLOBAL_FLAGS} -I/usr/lpp/cbclib/include")
+
+
+
+	target_compile_options(omr_shared INTERFACE "-Wc,DLL,EXPORTALL")
+
+	set(CMAKE_SHARED_LINKER_FLAGS "-Wl,xplink,dll")
+
+	# # This is the first option applied to the C++ linking command.
+	# # It is not applied to the C linking command.
+	# OMR_MK_CXXLINKFLAGS=-Wc,"langlvl(extended)" -+
+
+	# ifneq (,$(findstring shared,$(ARTIFACT_TYPE)))
+		# GLOBAL_LDFLAGS+=-Wl,xplink,dll
+	# else
+		# # Assume we're linking an executable
+		# GLOBAL_LDFLAGS+=-Wl,xplink
+	# endif
+	# ifeq (1,$(OMR_ENV_DATA64))
+		# OMR_MK_CXXLINKFLAGS+=-Wc,lp64
+		# GLOBAL_LDFLAGS+=-Wl,lp64
+	# endif
+
+	# # always link a2e last, unless we are creating the a2e library
+	# ifneq (j9a2e,$(MODULE_NAME))
+		# GLOBAL_SHARED_LIBS+=j9a2e
+	# endif
+
+	#dump DLLs and exes in same dir like on windows
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
+	#apparently above doesnt work like it does on windows. Attempt to work arround
+	set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}")
+	set(LIBRARY_OUTPUT_PATH  "${CMAKE_BINARY_DIR}")
+
+
+
+	message(STATUS "DEBUG: RUNTIME_OUTPUT_DIR=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+	message(STATUS "DEBUG: CFLAGS=${CMAKE_C_FLAGS}")
+	message(STATUS "DEBUG: EXE LDFLAGS=${CMAKE_EXE_LINKER_FLAGS}")
+	message(STATUS "DEBUG: so LDFLAGS=${CMAKE_SHARED_LINKER_FLAGS}")
+
+endif()
+
 ###
 ### Flags we aren't using
 ###
