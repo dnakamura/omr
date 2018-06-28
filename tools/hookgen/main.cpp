40aba34bfd38c9569a9f31a4de4107ae55b6cde2
Devin Nakamura <devinn@ca.ibm.com>
2018-06-27 23:13:26 -0400

    Fixups

--- tools/hookgen/main.cpp
+++ tools/hookgen/main.cpp
@@ -23,9 +23,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(J9ZOS390) && (__CHARSET_LIB != 1)
+#if defined(J9ZOS390)
+#if (__CHARSET_LIB != 1)
+#include <_Nascii.h>
+#else
 #include "atoe.h"
-#endif /* defined(J9ZOS390) && (__CHARSET_LIB != 1) */
+#endif
+#endif /* defined(J9ZOS390) */
 #if defined(OMR_OS_WINDOWS)
 #include <windows.h>
 #endif /* defined(OMR_OS_WINDOWS) */
@@ -41,7 +45,11 @@ main(int argc, char **argv, char **envp)
 #endif /* defined(OMR_OS_WINDOWS) */
 {
 	RCType rc = RC_OK;
-#if defined(J9ZOS390) && (__CHARSET_LIB != 1)
+#if defined(J9ZOS390)
+#if __CHARSET_LIB == 1
+	/* enable auto conversion */
+	__ae_autoconvert_state(_CVTSTATE_ON);
+#else
 	/* Convert EBCDIC to UTF-8 (ASCII) */
 	if (-1 != iconv_init()) {
 		/* translate argv strings to ascii */
@@ -57,6 +65,7 @@ main(int argc, char **argv, char **envp)
 		fprintf(stderr, "failed to initialize iconv\n");
 		rc = RC_FAILED;
 	}
+#endif
 #endif /* defined(J9ZOS390) && (__CHARSET_LIB != 1) */
 	if (RC_OK == rc) {
 		rc = startHookGen(argc, argv);
