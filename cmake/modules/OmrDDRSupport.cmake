802013ec7c11124da45a8959793fc04fa3e73a92
Devin Nakamura <devinn@ca.ibm.com>
2019-05-10 12:25:36 -0400

    Overhaul ddr

--- cmake/modules/OmrDDRSupport.cmake
+++ cmake/modules/OmrDDRSupport.cmake
@@ -30,15 +30,15 @@ include(ExternalProject)
 
 set(OMR_MODULES_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-function(make_ddr_set set_name)
+function(make_ddr_set ddr_set)
 	# if DDR is not enabled, just skip
 	# Also skip if we are on windows since it is unsupported at the moment
 	if((OMR_HOST_OS STREQUAL "win") OR (NOT OMR_DDR))
 		return()
 	endif()
-	set(DDR_TARGET_NAME "${set_name}_ddr")
-	set(DDR_BIN_DIR "${CMAKE_CURRENT_BINARY_DIR}/${DDR_TARGET_NAME}")
-	set(DDR_TARGETS_LIST "${DDR_BIN_DIR}/targets.list")
+	set(DDR_BIN_DIR "${CMAKE_CURRENT_BINARY_DIR}/${ddr_set}")
+	set(DDR_INFO_DIR "${CMAKE_BINARY_DIR}/ddr_info")
+	set(DDR_SET_INFO_FILE "${DDR_INFO_ROOT}/sets/${ddr_set}")
 	set(DDR_MACRO_INPUTS_FILE "${DDR_BIN_DIR}/macros.list")
 	set(DDR_TOOLS_EXPORT "${omr_BINARY_DIR}/ddr/tools/DDRTools.cmake")
 	set(DDR_CONFIG_STAMP "${DDR_BIN_DIR}/config.stamp")
@@ -49,48 +49,90 @@ function(make_ddr_set set_name)
 		COMMAND "${CMAKE_COMMAND}" -E touch "${DDR_CONFIG_STAMP}"
 		WORKING_DIRECTORY "${DDR_BIN_DIR}"
 	)
-	add_custom_target(${DDR_TARGET_NAME}
+	add_custom_target(${ddr_set}
 		DEPENDS "${DDR_CONFIG_STAMP}"
 		COMMAND ${CMAKE_COMMAND} --build "${DDR_BIN_DIR}"
 	)
-	set_property(TARGET "${DDR_TARGET_NAME}" PROPERTY DDR_BIN_DIR "${DDR_BIN_DIR}")
+
+	omr_join("\n" info_file_template
+		"NAME"
+		"${ddr_set}"
+		"BINARY_DIR"
+		"$<TARGET_PROPERTY:${ddr_set},BINARY_DIR>"
+		"TARGETS"
+		"$<JOIN:$<TARGET_PROPERTY:${ddr_set},DDR_TARGETS>,\n>"
+		"SUBSETS"
+		"$<TARGET_PROPERTY:${ddr_set},>"
+		"$<JOIN:$<TARGET_PROPERTY:${ddr_set},DDR_SUBSETS>,\n>"
+
+	)
+
+	file(GENERATE OUTPUT "${DDR_INFO_DIR}/sets/${ddr_set}")
+
+	# Note: DDR sets have themselves as targets to process
+	# This is so that you can process misc headers which dont logicly belong to any other target
+	target_enable_ddr(${ddr_set})
+	set_property(TARGET "${ddr_set}" APPEND PROPERTY DDR_TARGETS "${ddr_set}")
 
 	file(READ ${OMR_MODULES_DIR}/ddr/DDRSetStub.cmake.in cmakelist_template)
 	string(CONFIGURE "${cmakelist_template}" cmakelist_template @ONLY)
 	file(GENERATE OUTPUT ${DDR_BIN_DIR}/CMakeLists.txt CONTENT "${cmakelist_template}")
 endfunction(make_ddr_set)
 
-
-function(target_enable_ddr tgt ddr_set)
+function(ddr_set_add_targets ddr_set)
 	if((OMR_HOST_OS STREQUAL "win") OR (NOT OMR_DDR))
 		return()
 	endif()
 
+	omr_assert(FATAL_ERROR TEST TARGET "${ddr_set}" MESSAGE "ddrset_add_targets called on non-existant ddr-set ${ddr_set}")
+
+	foreach(tgt IN LISTS ARGN)
+		omr_assert(FATAL_ERROR TEST TARGET ${tgt} MESSAGE "ddrset_add_targets called on non-existant target ${tgt}")
+		# Check if the target we are adding is iteslf a DDR set
+		get_target_property(tgt_is_ddr_set ${tgt} DDR_SET)
+		if(tgt_is_ddr_set)
+			set_property(TARGET ${ddr_set} APPEND PROPERTY DDR_SUBSETS ${tgt})
+		else()
+			set_property(TARGET ${ddr_set} APPEND PROPERTY DDR_TARGETS ${tgt})
+		endif()
+		add_dependencies(${ddr_set} ${tgt})
+	endforeach()
+endfunction(ddr_set_add_targets)
+
+function(target_enable_ddr tgt)
+	if((OMR_HOST_OS STREQUAL "win") OR (NOT OMR_DDR))
+		return()
+	endif()
+
+	omr_assert(FATAL_ERROR TEST ARGC LESS 3 MESSAGE "target_enable_ddr called with wrong number of arguments")
+
 	set(opt_EARLY_SOURCE_EVAL )
+	set(opt_NO_SOURCE_EVAL )
 	set(opt_UNPARSED_ARGUMENTS )
-	cmake_parse_arguments(opt "EARLY_SOURCE_EVAL" "" "" ${ARGN})
+	cmake_parse_arguments(opt "EARLY_SOURCE_EVAL;NO_SOURCE_EVAL" "" "" ${ARGN})
 	omr_assert(FATAL_ERROR TEST NOT opt_UNPARSED_ARGUMENTS MESSAGE "target_enable_ddr: unrecognized options ${opt_UNPARSED_ARGUMENTS}")
 
-
-	set(DDR_SET_TARGET "${ddr_set}_ddr")
 	omr_assert(FATAL_ERROR TEST TARGET ${tgt} MESSAGE "target_enable_ddr called on non-existant target ${tgt}")
-	omr_assert(FATAL_ERROR TEST TARGET "${DDR_SET_TARGET}" MESSAGE "target_enable_ddr called on non-existant ddr_set ${ddr_set}")
 
 	get_target_property(target_type "${tgt}" TYPE)
 	if(target_type MATCHES "INTERFACE_LIBRARY")
 		message(FATAL_ERROR "Cannot call enable_ddr on interface libraries")
 	endif()
 
-	get_property(DDR_BIN_DIR TARGET "${DDR_SET_TARGET}" PROPERTY DDR_BIN_DIR)
 
-	if(opt_EARLY_SOURCE_EVAL)
-		set(source_property "DDR_EVAL_SOURCE")
-		get_target_property(sources "${tgt}" "SOURCES")
-		string(GENEX_STRIP "${sources}" cleaned_sources)
-		set_target_properties("${tgt}" PROPERTIES ${source_property} "${cleaned_sources}")
+	if(opt_EARLY_SOURCE_EVAL OR opt_NO_SOURCE_EVAL)
+		if(opt_EARLY_SOURCE_EVAL)
+			get_target_property(sources "${tgt}" "SOURCES")
+			string(GENEX_STRIP "${sources}" cleaned_sources)
+			set_target_properties("${tgt}" PROPERTIES ${source_property} "${cleaned_sources}")
+		endif()
+		set(source_list  "$<JOIN:$<TARGET_PROPERTY:${tgt},DDR_EVAL_SOURCE>,\n>")
+	elseif(opt_NO_SOURCE_EVAL)
+		set(source_list "")
 	else()
-		set(source_property "SOURCES")
+		set(source_list  "$<JOIN:$<TARGET_PROPERTY:${tgt},SOURCES>,\n>")
 	endif()
+
 	omr_join("\n" MAGIC_TEMPLATE
 		"SOURCE_DIR"
 		"$<TARGET_PROPERTY:${tgt},SOURCE_DIR>"
@@ -99,7 +141,7 @@ function(target_enable_ddr tgt ddr_set)
 		"DEFINES"
 		"$<JOIN:$<TARGET_PROPERTY:${tgt},COMPILE_DEFINITIONS>,\n>"
 		"SOURCES"
-		"$<JOIN:$<TARGET_PROPERTY:${tgt},${source_property}>,\n>"
+		"${source_list}"
 		"HEADERS"
 		"$<JOIN:$<TARGET_PROPERTY:${tgt},DDR_HEADERS>,\n>"
 		"PREINCLUDES"
@@ -109,6 +151,10 @@ function(target_enable_ddr tgt ddr_set)
 		set(MAGIC_TEMPLATE "OUTPUT_FILE\n$<TARGET_FILE:${tgt}>\n${MAGIC_TEMPLATE}")
 	endif()
 
-	file(GENERATE OUTPUT "${DDR_BIN_DIR}/${tgt}.txt" CONTENT "${MAGIC_TEMPLATE}\n")
-	set_property(TARGET ${DDR_SET_TARGET} APPEND PROPERTY INPUT_TARGETS ${tgt})
+	file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/ddr_info/targets/${tgt}.txt" CONTENT "${MAGIC_TEMPLATE}\n")
+
+	# if we were given a DDR set, add this target to it
+	if(ARGC EQUAL 2)
+		ddr_set_add_targets(${ARGV1} ${tgt})
+	endif()
 endfunction(target_enable_ddr)
