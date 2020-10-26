bf94ed41c9ce1691ccfae5c087a7a3a6c5b1affe
Devin Nakamura <devinn@ca.ibm.com>
2020-10-26 13:32:04 -0400

    CMake: Fix handling of ddr scanner
    - Enable support for z/OS
    - Don't compile DwarfFunctions on linux (matches behaviour of makefiles)
    
    Signed-off-by: Devin Nakamura <devinn@ca.ibm.com>

--- cmake/modules/platform/os/zos.cmake
+++ cmake/modules/platform/os/zos.cmake
@@ -38,6 +38,9 @@ list(APPEND OMR_PLATFORM_INCLUDE_DIRECTORIES
 	/usr/include
 )
 
+# Make sure that cmake can find libelf/libdwarf headers.
+list(APPEND CMAKE_INCLUDE_PATH "/usr/lpp/cbclib/include")
+
 # Create helper targets for specifying ascii/ebcdic options
 add_library(omr_ascii INTERFACE)
 target_compile_definitions(omr_ascii INTERFACE -DIBM_ATOE)
